c404063b889aa67e852f678bef1cec7f65da5b55
malec <alec@ahlbrandt.net>
2019-04-16 20:57:45 -0500

    add cube for player

--- project5.cpp
+++ project5.cpp
@@ -97,6 +97,85 @@ void block(float xmin, float ymin, float zmin, float xmax, float ymax, float zma
 	glEnd();
 }
 
+void cube(float midx, float midy, float midz, float size, float r, float g, float b)
+{
+	// Define 8 vertices
+	float ax = midx - size / 2;
+	float ay = midy - size / 2;
+	float az = midz + size / 2;
+	float bx = midx + size / 2;
+	float by = midy - size / 2;
+	float bz = midz + size / 2;
+	float cx = midx + size / 2;
+	float cy = midy + size / 2;
+	float cz = midz + size / 2;
+	float dx = midx - size / 2;
+	float dy = midy + size / 2;
+	float dz = midz + size / 2;
+	float ex = midx - size / 2;
+	float ey = midy - size / 2;
+	float ez = midz - size / 2;
+	float fx = midx + size / 2;
+	float fy = midy - size / 2;
+	float fz = midz - size / 2;
+	float gx = midx + size / 2;
+	float gy = midy + size / 2;
+	float gz = midz - size / 2;
+	float hx = midx - size / 2;
+	float hy = midy + size / 2;
+	float hz = midz - size / 2;
+
+	// Draw 6 faces
+	glBegin(GL_POLYGON);
+	glColor3f(r, g, b);
+	glVertex3f(ax, ay, az);
+	glVertex3f(bx, by, bz);
+	glVertex3f(cx, cy, cz);
+	glVertex3f(dx, dy, dz);
+	glEnd();
+
+	glBegin(GL_POLYGON);
+	glColor3f(r - .1, g - .1, b - .1);
+	glVertex3f(ax, ay, az);
+	glVertex3f(dx, dy, dz);
+	glVertex3f(hx, hy, hz);
+	glVertex3f(ex, ey, ez);
+	glEnd();
+
+	glBegin(GL_POLYGON);
+	glColor3f(r - .04, g - .04, b - .04);
+	glVertex3f(ax, ay, az);
+	glVertex3f(ex, ey, ez);
+	glVertex3f(fx, fy, fz);
+	glVertex3f(bx, by, bz);
+	glEnd();
+
+	glBegin(GL_POLYGON);
+	glColor3f(r + .04, g + .04, b + .04);
+	glVertex3f(gx, gy, gz);
+	glVertex3f(fx, fy, fz);
+	glVertex3f(ex, ey, ez);
+	glVertex3f(hx, hy, hz);
+	glEnd();
+
+	glBegin(GL_POLYGON);
+	glColor3f(r - .12, g - .12, b - .12);
+
+	glVertex3f(gx, gy, gz);
+	glVertex3f(cx, cy, cz);
+	glVertex3f(bx, by, bz);
+	glVertex3f(fx, fy, fz);
+	glEnd();
+
+	glBegin(GL_POLYGON);
+	glColor3f(r - .2, g - .2, b - .2);
+	glVertex3f(gx, gy, gz);
+	glVertex3f(hx, hy, hz);
+	glVertex3f(dx, dy, dz);
+	glVertex3f(cx, cy, cz);
+	glEnd();
+}
+
 float xAngle = 0;
 float yAngle = 0;
 float zAngle = 0;
@@ -197,8 +276,7 @@ void display()
 	// draw the player
 	auto position = player.getPlayerPosition();
 	printf("player position: %f, %f, %f\n", position[0], position[1], position[2]);
-	auto maxPosition = player.getPlayerMaxPosition();
-	block(position[0], position[1], position[2], maxPosition[0], maxPosition[1], maxPosition[2]);
+	cube(position[0], position[1], position[2], .0625, (float)201 / 255, (float)66 / 255, (float)68 / 255);
 	// glPushMatrix();
 	for (int z = 0; z < maze.getRowCount(); z++)
 	{
@@ -207,8 +285,8 @@ void display()
 			int height = 3;
 			char material = maze.getBlockMaterial(z, x);
 			if (material != ' ')
-				printf("%i, %i\n", z, x);
 			{
+				printf("%i, %i\n", z, x);
 				float xCoord = -.5 + x * maze.getBlockXSize();
 				float yCoord = height * maze.getBlockYSize();
 				float zCoord = .5 - z * maze.getBlockZSize();
